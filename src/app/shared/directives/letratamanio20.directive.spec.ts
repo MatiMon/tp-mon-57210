80c0f8d8fc14e0627649c75b2237868788caa6ca
Mati Mon <matias.mon@bizitglobalarg.onmicrosoft.com>
2024-08-11 18:14:03 -0300

    Elimino tests rotos

--- src/app/shared/directives/letratamanio20.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Letratamanio20Directive } from './letratamanio20.directive';
-
-describe('Letratamanio20Directive', () => {
-  it('should create an instance', () => {
-    const directive = new Letratamanio20Directive();
-    expect(directive).toBeTruthy();
-  });
-});
